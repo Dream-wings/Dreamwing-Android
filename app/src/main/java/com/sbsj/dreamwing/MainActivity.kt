569a241360ab251b5c4dfc940f22dd75c071c5aa
eunjeeing <ejjeongggg@gmail.com>
2024-08-01 01:31:51 +0900

    Feat : 네비게이션 바 구현

--- app/src/main/java/com/sbsj/dreamwing/MainActivity.kt
+++ app/src/main/java/com/sbsj/dreamwing/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sbsj.dreamwing.volunteer.VolunteerActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,14 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             val intent = Intent(this, VolunteerActivity::class.java)
             startActivity(intent)
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
         }
     }
 }
