59f0043dc21e59ffa85abfe1fcb22234116a7ab1
eunjeeing <ejjeongggg@gmail.com>
2024-07-31 22:33:12 +0900

    Chore : API URL 숨김 처리 설정

--- app/src/main/java/com/sbsj/dreamwing/data/api/RetrofitClient.kt
+++ app/src/main/java/com/sbsj/dreamwing/data/api/RetrofitClient.kt
@@ -1,17 +1,17 @@
 package com.sbsj.dreamwing.data.api
 
+import com.sbsj.dreamwing.BuildConfig
 import com.sbsj.dreamwing.support.service.SupportService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2"  // http://localhost:
+    private const val BASE_URL = BuildConfig.BASE_URL
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-
 
     val instance: ApiService by lazy {
 //        val retrofit = Retrofit.Builder()
