e02c4fea9d66014675d6d70b40aeefff96992d97
eunjeeing <ejjeongggg@gmail.com>
2024-08-02 11:50:20 +0900

    Feat : 미션 목록 화면 구현

---
+++ app/src/main/java/com/sbsj/dreamwing/mission/ui/WalkActivity.kt
@@ -0,0 +1,12 @@
+package com.sbsj.dreamwing.mission.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sbsj.dreamwing.R
+
+class WalkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walk)
+    }
+}
