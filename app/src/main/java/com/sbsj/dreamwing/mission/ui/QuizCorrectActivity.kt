a19d52230d4300e4f623a78fbb60c2b7797ffa22
eunjeeing <ejjeongggg@gmail.com>
2024-08-02 21:01:21 +0900

    Feat : 데일리 퀴즈 화면 구현

--- app/src/main/java/com/sbsj/dreamwing/mission/ui/QuizCorrectActivity.kt
+++ app/src/main/java/com/sbsj/dreamwing/mission/ui/QuizCorrectActivity.kt
@@ -1,21 +1,67 @@
 package com.sbsj.dreamwing.mission.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.sbsj.dreamwing.R
+import com.sbsj.dreamwing.common.model.ApiResponse
+import com.sbsj.dreamwing.data.api.RetrofitClient
+import com.sbsj.dreamwing.databinding.ActivityQuizBinding
+import com.sbsj.dreamwing.databinding.ActivityQuizCorrectBinding
+import com.sbsj.dreamwing.mission.model.ActivityType
+import com.sbsj.dreamwing.mission.model.request.AwardPointRequest
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class QuizCorrectActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityQuizCorrectBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_quiz_correct)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityQuizCorrectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar.root)
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        awardPoints()
+    }
+
+    // 툴바 뒤로가기 버튼
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun awardPoints() {
+        val request = AwardPointRequest(
+            userId = 1,
+            activityType = ActivityType.QUIZ.type,
+            activityTitle = ActivityType.QUIZ.displayName,
+            point = ActivityType.QUIZ.point
+        )
+
+        RetrofitClient.missionService.awardPoints(request).enqueue(object :
+            Callback<ApiResponse<Any>> {
+            override fun onResponse(call: Call<ApiResponse<Any>>, response: Response<ApiResponse<Any>>) {
+                if (response.isSuccessful) {
+                    Log.d("QuizCorrectActivity", "Response: ${response.body()}")
+                } else {
+                    Log.e("QuizCorrectActivity", "Response not successful: ${response.errorBody()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponse<Any>>, t: Throwable) {
+                Log.e("QuizCorrectActivity", "Request failed: ${t.message}")
+            }
+        })
     }
 }
