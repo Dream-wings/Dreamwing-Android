48fc4cce5d86a56bd2f5595fb1f87b5fc73c11a4
Eunchan Jeong <jec65147@gmail.com>
2024-08-03 12:48:55 +0900

    Feat : 로그인 완료 혹은 실패시 다이얼로그 호출

--- app/src/main/java/com/sbsj/dreamwing/user/LoginActivity.kt
+++ app/src/main/java/com/sbsj/dreamwing/user/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.sbsj.dreamwing.user
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -9,10 +11,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sbsj.dreamwing.MainActivity
 import com.sbsj.dreamwing.R
 import com.sbsj.dreamwing.data.api.RetrofitClient
 import com.sbsj.dreamwing.user.model.dto.LoginRequestDTO
 import com.sbsj.dreamwing.user.model.response.CheckExistIdResponse
+import com.sbsj.dreamwing.user.model.response.SignUpResponse
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -27,7 +31,8 @@ import retrofit2.Response
  *
  * 수정일        	수정자        수정내용
  * ----------  --------    ---------------------------
- * 2024.08.01   정은찬        최초 생성
+ * 2024.08.02   정은찬        최초 생성
+ * 2024.08.03   정은찬        Rtrofit을 통해 로그인 정보 보내기 및 JWT 토큰 헤더에서 받기
  */
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,8 +52,14 @@ class LoginActivity : AppCompatActivity() {
             if (loginIdText.isNotEmpty() && passwordText.isNotEmpty()) {
                 login(loginIdText, passwordText)
             } else {
-                // TODO: 유효성 검사 실패 처리 (예: 사용자에게 입력하라고 알림)
+                showErrorDialog()
             }
+        }
+
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
@@ -56,22 +67,19 @@ class LoginActivity : AppCompatActivity() {
         // 요청 바디 생성
         val loginRequestDTO = LoginRequestDTO(loginId, password)
 
-        RetrofitClient.userService.login(loginRequestDTO).enqueue(object :
-            Callback<Void> {
+        RetrofitClient.userService.login(loginRequestDTO).enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 if (response.isSuccessful) {
                     // 성공적으로 로그인한 경우 Authorization 헤더에서 JWT 토큰 추출
                     val token = response.headers()["Authorization"]
                     if (token != null) {
-                        // JWT 토큰을 SharedPreferences에 저장
-                        saveToken(token)
-                        Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        showLoginSuccessDialog()
                     } else {
                         Toast.makeText(this@LoginActivity, "토큰을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
                     }
                 } else {
                     // 로그인 실패 시 사용자에게 알림
-                    Toast.makeText(this@LoginActivity, "로그인 실패: ${response.message()}", Toast.LENGTH_SHORT).show()
+                    showLoginFailDialog()
                 }
             }
 
@@ -91,5 +99,36 @@ class LoginActivity : AppCompatActivity() {
         val editor = sharedPreferences.edit()
         editor.putString("token", token)
         editor.apply()
+    }
+
+    // 오류 다이얼로그를 표시하는 메서드
+    private fun showErrorDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("오류")
+            .setMessage("로그인 정보를 입력해주세요.")
+            .setPositiveButton("확인") { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    // 로그인 성공 다이얼로그를 표시하는 메서드
+    private fun showLoginSuccessDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("로그인 성공")
+            .setMessage("로그인이 성공적으로 완료되었습니다.")
+            .setPositiveButton("확인") { dialog, _ ->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // 현재 액티비티 종료
+            }
+            .show()
+    }
+
+    // 로그인 실패 다이얼로그를 표시하는 메서드
+    private fun showLoginFailDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("로그인 실패")
+            .setMessage("로그인 정보를 다시 입력해주세요.")
+            .setPositiveButton("확인") { dialog, _ -> dialog.dismiss() }
+            .show()
     }
 }
