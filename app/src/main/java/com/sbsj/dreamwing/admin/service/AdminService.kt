e8fc9b72f5bfed9e1f52b524c2a702b2b65d3a10
eunjeeing <ejjeongggg@gmail.com>
2024-08-05 04:00:37 +0900

    Feat : 관리자 봉사활동 신청 대기 리스트 화면 구현

---
+++ app/src/main/java/com/sbsj/dreamwing/admin/service/AdminService.kt
@@ -0,0 +1,40 @@
+package com.sbsj.dreamwing.admin.service
+
+import com.sbsj.dreamwing.admin.model.response.VolunteerRequestListResponse
+import com.sbsj.dreamwing.common.model.ApiResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * 관리자 서비스 인터페이스
+ * @author 정은지
+ * @since 2024.08.04
+ * @version 1.0
+ *
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.04   정은지        최초 생성
+ */
+interface AdminService {
+
+    /**
+     * 봉사활동 신청 대기 목록
+     * @author 정은지
+     * @since 2024.08.04
+     * @version 1.0
+     */
+    @GET("/admin/volunteer/request/list")
+    fun getVolunteerRequestList(@Query("page") page: Int,
+                                @Query("size") size: Int) : Call<ApiResponse<List<VolunteerRequestListResponse>>>
+
+    /**
+     * 봉사활동 신청 상세
+     * @author 정은지
+     * @since 2024.08.04
+     * @version 1.0
+     */
+//    @GET("/admin/volunteer/request")
+//    fun getVolunteerRequestDetail(@Query("volunteerId") volunteerId: Long,
+//                                  @Query("userId") userId: Long)
+}
