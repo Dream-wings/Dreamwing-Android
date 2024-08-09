e3090ecc2fdded380a02fcb1a1104143d9e4954a
JAESEONGIM <bigstar2878@gmail.com>
2024-08-09 16:41:03 +0900

    Refactor : 봉사도메인, 관리자 도메인 주석 추가

--- app/src/main/java/com/sbsj/dreamwing/admin/model/response/VolunteerAdminListResponse.kt
+++ app/src/main/java/com/sbsj/dreamwing/admin/model/response/VolunteerAdminListResponse.kt
@@ -1,7 +1,17 @@
 package com.sbsj.dreamwing.admin.model.response
 
 import com.google.gson.annotations.SerializedName
-
+/**
+ * 관리자 페이지에서 봉사활동 목록 API의 응답을 나타내는 데이터 전송 객체 (DTO)
+ * @author 임재성
+ * @since 2024.08.05
+ * @version 1.0
+ *
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.05   임재성        최초 생성
+ * 2024.08.05   임재성        봉사활동 목록 데이터 필드 추가
+ */
 data class VolunteerAdminListResponse(
     @SerializedName("code") val code: Int,
     @SerializedName("success") val success: Boolean,
--- app/src/main/java/com/sbsj/dreamwing/volunteer/model/response/VolunteerDetailResponse.kt
+++ app/src/main/java/com/sbsj/dreamwing/volunteer/model/response/VolunteerDetailResponse.kt
@@ -3,6 +3,7 @@ package com.sbsj.dreamwing.volunteer.model.response
 import com.google.gson.annotations.SerializedName
 import com.sbsj.dreamwing.volunteer.model.VolunteerDetailDTO
 
+
 data class VolunteerDetailResponse(
     @SerializedName("code") val code: Int,
     @SerializedName("success") val success: Boolean,
